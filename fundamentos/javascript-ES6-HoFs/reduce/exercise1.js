f256f00991ea6af459cb03f114472f96b42ea20c
matheus-amsfer <matheus.cid47@gmail.com>
2022-11-10 14:34:57 -0300

    Exercicio completo

---
+++ fundamentos/javascript-ES6-HoFs/reduce/exercise1.js
@@ -0,0 +1,117 @@
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+  ];
+  
+  function flatten(arrays) {
+   return arrays.reduce((acc, cur) => acc.concat(cur));
+  }
+//   console.log(flatten(arrays));
+
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+//   const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+function reduceNames(books) {
+  return `"${books.reduce((acc, cur) => acc + `${cur.author.name}, `, '')}"`
+}
+// console.log(reduceNames(books));
+
+// const expectedResult = 43;
+
+function averageAge(books) {
+  return books.reduce((acc, cur) => acc + cur.releaseYear - cur.author.birthYear, 0) / books.length;
+}
+// console.log(averageAge(books));
+
+
+// const expectedResult = {
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     genre: 'Fantasia',
+//     author: {
+//       name: 'George R. R. Martin',
+//       birthYear: 1948,
+//     },
+//     releaseYear: 1991,
+//   };
+  
+  function longestNamedBook(books) {
+    return books.reduce((acc, cur) => acc.name.length > cur.name.length ? acc : cur);
+  }
+     
+//   console.log(longestNamedBook(books));
+
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
+  
+  const expectedResult = 20;
+  
+  function containsA() {
+    
+  }
